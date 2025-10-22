97b16eff266aec00348b7e1220c9ba873da2c12f
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-22 03:22:58 -0300

    Part 1

---
+++ Part 01/Reading input/Story.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Story {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String CharacterName = input.nextLine().trim();
+
+        System.out.println("What is their job?");
+        String JobName = input.nextLine().trim();
+
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + CharacterName + ", who was " + JobName + ".");
+        System.out.println("On the way to work, " + CharacterName + " reflected on life.");
+        System.out.println("Perhaps " + CharacterName + " will not be " + JobName + " forever.");
+
+    }
+
+}
