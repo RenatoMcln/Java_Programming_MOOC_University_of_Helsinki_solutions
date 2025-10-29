e049b4bce35a5f1e8ecdbc5804d44bad89e745d6
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-29 15:30:04 -0300

    Java Programming I

---
+++ Part 01/06 - Conditional statements and conditional operation/Orwell.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Orwell {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give an integer number:");
+        long number = scan.nextInt();
+
+        if (number == 1984) {
+            System.out.println("Orwell");
+        }
+
+    }
+}
